d2661277fd7c4085f9d9a1954f966f6a1a1ed508
Samiksha <samikshachavan.dev@gmail.com>
2026-02-15 22:03:53 +0530

    singly linked list operations

--- DSA/src/com/linkedlist/singly/Main.java
+++ DSA/src/com/linkedlist/singly/Main.java
@@ -21,12 +21,88 @@ public class Main {
 		}
 	}
 	
+	public static Node deleteAtHead(Node head) {
+		head=head.next;
+		return head;
+	}
+	
+	public static Node deleteAtTail(Node head) {
+		Node temp=head;
+		while(temp.next.next!=null) {
+			temp=temp.next;
+		}
+		temp.next=null;
+		return head;
+	}
+	
+	public static Node deleteAtK(Node head, int k) {
+		Node prev=null;
+		Node temp=head;
+		int cnt=0;
+		
+		while(temp!=null) {
+			if(k-1==cnt) {
+				if(prev!=null && prev.next!=null) {
+					prev.next=prev.next.next;
+					break;
+				}
+			}
+			prev=temp;
+			temp=temp.next;
+			cnt++;
+		}
+		return head;
+		
+	}
+	
+	
+	public static Node insertAtHead(Node head, int data) {
+		Node newNode=new Node(data,head);
+		return newNode;
+	}
+	
+	public static Node insertAtTail(Node head, int data) {
+		Node temp=head;
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		Node newNode=new Node(data);
+		temp.next=newNode;
+		
+		return head;
+	}
+	
+	public static Node insertAtK(Node head, int k, int data) {
+		Node prev=null;
+		Node temp=head;
+		int cnt=0;
+		while(temp!=null) {
+			if(k-1==cnt) {
+				if(prev!=null&&prev.next!=null) {
+					Node newNode=new Node(data, prev.next);
+					prev.next=newNode;
+				}
+			}
+			prev=temp;
+			temp=temp.next;
+			cnt++;
+		}
+		return head;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int[] arr = { 10, 20, 30, 40, 50 };
 		Node head=convertArr(arr);
-		displayNode(head);
+//		Node newNode=deleteAtHead(head);
+//		Node newNode=insertAtHead(head,100);
+//		Node newNode=deleteAtTail(head);
+//		Node newNode=insertAtTail(head,100);
+//		Node newNode=deleteAtK(head, 3);
+		Node newNode=insertAtK(head,3,100);
+		displayNode(newNode);
+		
+		
 
 	}
 
